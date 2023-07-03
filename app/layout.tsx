d68d1fd7e3fdecec6e014f1c35b790683eff11dc
Sandip Thapa <sandipthapa386@gmail.com>
2023-07-03 15:54:07 +0545

    feat(Navbar): Navbar layout skeleton

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,6 @@
+import Navbar from "@/components/Navbar";
+import "./globals.css";
+import Footer from "@/components/Footer";
 export const metadata = {
     title: "Flexibble",
     description: "Showcase and discover remarkable developer projects",
@@ -7,7 +10,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     return (
         <html lang="en">
             <body>
+                <Navbar />
                 <main>{children}</main>
+                <Footer />
             </body>
         </html>
     );
