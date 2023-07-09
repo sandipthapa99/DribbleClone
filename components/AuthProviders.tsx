d864ab2803c06a74c000de420e6e692c852e8e0c
Sandip Thapa <sandipthapa386@gmail.com>
2023-07-09 15:36:45 +0545

    feat(Next Auth): Next Auth basic setup with session config

--- components/AuthProviders.tsx
+++ components/AuthProviders.tsx
@@ -1,6 +1,38 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
+import { getProviders, signIn } from "next-auth/react";
+
+type Provider = {
+    id: string;
+    name: string;
+    type: string;
+    signinUrl: string;
+    callbackUrl: string;
+    signinUrlParams?: Record<string, string> | null;
+};
+
+type Providers = Record<string, Provider>;
 
 const AuthProviders = () => {
+    const [providers, setProviders] = useState<Providers | null>(null);
+
+    useEffect(() => {
+        const fetchProviders = async () => {
+            const res = await getProviders();
+            setProviders(res);
+        };
+    }, []);
+
+    if (providers) {
+        return (
+            <div>
+                {Object.values(providers).map((provider: Provider, index) => (
+                    <button key={index}>{provider.id}</button>
+                ))}
+            </div>
+        );
+    }
     return <div>AuthProviders</div>;
 };
 
